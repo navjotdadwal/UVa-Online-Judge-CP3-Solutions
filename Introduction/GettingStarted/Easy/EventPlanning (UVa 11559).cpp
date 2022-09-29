c71ba78324cafde8d37b059325822aaf46003723
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2022-09-29 10:43:49 +0530

    Create EventPlanning (UVa 11559).cpp

---
+++ Introduction/GettingStarted/Easy/EventPlanning (UVa 11559).cpp
@@ -0,0 +1,73 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>  
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#define trace1(x) cerr<<#x<<x<<endl
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+#define all(x) (x).begin(),(x).end()
+#define int long long int
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define F first
+#define S second
+#define inf 1e18
+#define vi vector<int>
+#define Point complex<int>
+#define X real()
+#define Y imag()
+#define pii pair<int,int>
+#define num0Beg(x) __builtin_clz(x) // no. of zero in beginning of bit representation
+#define num0end(x) __builtin_ctz(x) // no. of zero in the end
+#define numOf1(x) __builtin_popcount(x) // no. of ones in bit rep.
+#define parity(x) __builtin_parity(x) // parity of 1
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+
+int32_t main()
+{
+    FAST
+    int tt=1;
+    int n,h,b,w;
+    while(cin>>n>>b>>h>>w)
+    {         
+      vector<int> pr(h);
+      vector<vector<int>> v(h,vector<int>(w));
+      int ans=inf;
+      for(int i=0;i<h;i++)
+      {
+        cin>>pr[i];
+        for(int j=0;j<w;j++)
+        {
+          cin>>v[i][j];
+          if(v[i][j]>=n)
+            ans=min(ans,n*pr[i]);  
+        }
+      }
+      if(ans>b)
+        cout<<"stay home\n";
+      else 
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
